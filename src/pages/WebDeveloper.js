87045a9789d2e9f925ae78a82b40a45e41742074
mowzayo <mosesayodelee11@gmail.com>
2024-12-18 21:43:05 +0100

    Initial commit

---
+++ src/pages/WebDeveloper.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+
+function WebDeveloper(){
+    return (
+    <div> 
+         <h1> WEB JOBS I'VE HANDLED </h1> 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </div>
+    );
+}
+
+export default WebDeveloper;
